9c19bd71bedfc7e9e2ca16898dfe8059fe52c3ee
Vijay <vjvijay2k@gmail.com>
2022-03-28 21:47:32 +0530

    updated

---
+++ AOD.NET_Assignments/AOD.NET_Assignments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOD.NET_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ AOD.NET_Assignments/AOD.NET_Assignments/Q76_InsertStudentData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOD.NET_Assignments
+{
+    internal class Q76_InsertStudentData
+    {
+    }
+}
